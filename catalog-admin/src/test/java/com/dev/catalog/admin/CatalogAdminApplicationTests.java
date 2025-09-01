10c95656fdfa06a2921de169aa1e7615008974a4
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2025-09-01 19:08:40 -0400

    Initialize catalog-admin project with Gradle setup and basic Spring Boot application structure

---
+++ catalog-admin/src/test/java/com/dev/catalog/admin/CatalogAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.catalog.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
