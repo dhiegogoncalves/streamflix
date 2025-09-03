0243088ec0c1fb044e0c98343fe0f23c58bb9436
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2025-09-02 22:57:56 -0400

    Add Category and CategoryID classes with corresponding tests

--- catalog-admin/domain/src/main/java/com/dev/catalog/admin/domain/Category.java
+++
@@ -1,6 +0,0 @@
-
-package com.dev.catalog.admin.domain;
-
-public class Category {
-    public String id;
-}
---
+++ catalog-admin/domain/src/main/java/com/dev/catalog/admin/domain/category/Category.java
@@ -0,0 +1,61 @@
+
+package com.dev.catalog.admin.domain.category;
+
+import java.time.Instant;
+import com.dev.catalog.admin.domain.AggregateRoot;
+
+public class Category extends AggregateRoot<CategoryID> {
+    private String name;
+    private String description;
+    private boolean active;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private Instant deletedAt;
+
+    private Category(final CategoryID id, final String name, final String description,
+            final boolean active, final Instant createdAt, final Instant updatedAt,
+            final Instant deletedAt) {
+        super(id);
+        this.name = name;
+        this.description = description;
+        this.active = active;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
+
+    public static Category newCategory(final String name, final String description,
+            final boolean isActive) {
+        final var id = CategoryID.unique();
+        final var now = Instant.now();
+        return new Category(id, name, description, isActive, now, now, null);
+    }
+
+    public CategoryID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Instant getDeletedAt() {
+        return deletedAt;
+    }
+}
